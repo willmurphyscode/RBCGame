d3b597c3526205b2d2d3f3ec7510329c9648db97
Will Murphy <willmurphyscode@gmail.com>
2017-03-31 21:05:57 -0400

    Rename @sprite to @player_sprite

--- game_state.rb
+++ game_state.rb
@@ -6,21 +6,21 @@ class GameState
   end
 
   def set_up_sprites
-    @sprite = PlayerSprite.new @window
+    @player_sprite = PlayerSprite.new @window
     @floor = FloorSprite.new @window
     wall1 = WallSprite.new @window, { height: 200, width: 85, x: 200, y:400 }
     @walls = [wall1]
   end
 
   def tick
-    @sprite.update
-    player_rectangle = @sprite.rectangle
+    @player_sprite.update
+    player_rectangle = @player_sprite.rectangle
     wall_rectangle = @walls[0].rectangle
-    puts wall_rectangle.collides? player_rectangle
+    wall_rectangle.collides? player_rectangle
   end
 
   def draw
-    @sprite.draw
+    @player_sprite.draw
     @floor.draw
     @walls.each {|w| w.draw}
   end
