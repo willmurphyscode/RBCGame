2eb26a2b3e3547892bd33df958735ff8dbbced6f
Will Murphy <willmurphyscode@gmail.com>
2017-04-08 10:18:09 -0400

    Override x_min, y_max, etc, on player
    The player sprites image has some transparency around it.
    It will detect collisions early if we use the true edge of the
    image. This overrides the rectangle methods to draw a smaller
    rectangle around the picture.

--- player_sprite.rb
+++ player_sprite.rb
@@ -24,6 +24,22 @@ class PlayerSprite
     @moving = false
   end
 
+  def x_min
+    @x + 25
+  end
+
+  def y_min
+    @y + 25
+  end
+
+  def x_max
+    @x + @width - 25
+  end
+
+  def y_max
+    @y + @height - 25
+  end
+
   def update
     @frame += 1
     @moving = false
